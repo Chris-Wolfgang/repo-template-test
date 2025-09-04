bc477c9cae4a019f1989e036a376876e878f8fb8
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-03 21:44:42 -0400

    Added more security issues

---
+++ src/Repository-Template-Test/InsecureQuery.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+public class InsecureQuery
+{
+    public void GetUser(string username)
+    {
+        var connectionString = "your-connection-string-here";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            // Vulnerable to SQL injection!
+            string sql = "SELECT * FROM Users WHERE Username = '" + username + "'";
+            SqlCommand command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+        }
+    }
+}
