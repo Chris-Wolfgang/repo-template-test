0f60b884a6cba74d8b5e7d21cd59b29dc2905edb
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-03 21:24:36 -0400

    Added more vulnerability

--- src/Repository-Template-Test/ProgramApp.cs
+++ src/Repository-Template-Test/ProgramApp.cs
@@ -13,5 +13,10 @@ public class ProgramApp
             var sqlCommandText = $" SELECT * FROM {args[0]} WHERE ColumnA = '{args[1]}'";
             Console.WriteLine($"Arguments provided: {string.Join(", ", args)}");
         }
+
+        var username = "admin";
+        string sql = "SELECT * FROM Users WHERE Username = '" + username + "'";
+        string password = "SuperSecret123!";
+        var md5 = System.Security.Cryptography.MD5.Create();
     }
 }
