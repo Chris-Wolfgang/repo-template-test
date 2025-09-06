a27d3fc67cb089be45e7d2100691b5e0bf6816b0
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2025-09-05 21:51:13 -0400

    Added additional security issues

--- src/Repository-Template-Test/ProgramApp.cs
+++ src/Repository-Template-Test/ProgramApp.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using Microsoft.Data.SqlClient;
+
 namespace RepositoryTemplateTest.App;
 
 public class ProgramApp
@@ -14,7 +17,35 @@ public class ProgramApp
             Console.WriteLine($"Arguments provided: {string.Join(", ", args)}");
         }
 
-        var insecure = new InsecureQuery();
-        insecure.GetUser("admin' --");
+        string connectionString = "Server=myserver;Database=mydb;User Id=admin;Password=SuperSecret123;";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            // Vulnerable to SQL injection!
+            string sql = "SELECT * FROM Users WHERE Username = '" + args[0] + "'";
+            SqlCommand command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+        }
+    }
+
+    void ConnectWithHardcodedPassword()
+    {
+        string password = "SuperSecret123"; // DevSkim should flag this
+        Console.WriteLine($"Password: {password}");
+    }
+
+    void UseInsecureCrypto()
+    {
+        var des = DES.Create(); // DevSkim will flag use of DES as insecure
+        byte[] key = des.Key;
+        byte[] iv = des.IV;
+    }
+
+
+    void SqlInjectionRisk(string userInput)
+    {
+        string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'"; // DevSkim will flag this
+        Console.WriteLine(query);
     }
 }
---
+++ src/SecurityScanTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+void ConnectWithHardcodedPassword()
+{
+    string password = "SuperSecret123"; // DevSkim should flag this
+    Console.WriteLine($"Password: {password}");
+}
+
+
+void UseInsecureCrypto()
+{
+    var des = DES.Create(); // DevSkim will flag use of DES as insecure
+    byte[] key = des.Key;
+    byte[] iv = des.IV;
+}
+
+
+void SqlInjectionRisk(string userInput)
+{
+    string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'"; // DevSkim will flag this
+    Console.WriteLine(query);
+}
